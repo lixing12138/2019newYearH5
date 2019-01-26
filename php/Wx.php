0711f9303d5b9c120d8db37b9f9a0b7f4d960d3c
lixing <16302010012@fudan.edu.cn>
2019-01-26 11:35:33 +0800

    fenxiang

--- php/Wx.php
+++ php/Wx.php
@@ -8,72 +8,78 @@
 
 class Wx
 {
-    protected $appid='wxff5e0192690a209f';
-    protected $secret='fd5ffc15024856360d191b8ed04d2c8d';
+    protected $appid = 'wxff5e0192690a209f';
+    protected $secret = 'fd5ffc15024856360d191b8ed04d2c8d';
 
-    public function getAccessToken(){
-        $name='token_'.md5($this->appid.$this->secret);
-        $filename=__DIR__.'/cache/'.$name.'.php';
-        if (is_file($filename)&&filemtime($filename) + 7100>time()){
-            $res=include $filename;
-            $data=$res['access_token'];
-        }else{
-            $url='https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->appid.'&secret='.$this->secret;
-            $result=$this->curl($url);
-            $res=json_decode($result,true);
-            file_put_contents($filename,"<?php \n return ".var_export($res,true).";\n?>");
+    public function getAccessToken()
+    {
+        $name = 'token_' . md5($this->appid . $this->secret);
+        $filename = __DIR__ . '/cache/' . $name . '.php';
+        if (is_file($filename) && filemtime($filename) + 7100 > time()) {
+            $res = include $filename;
+            $data = $res['access_token'];
+        } else {
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $this->appid . '&secret=' . $this->secret;
+            $result = $this->curl($url);
+            $res = json_decode($result, true);
+            file_put_contents($filename, "<?php \n return " . var_export($res, true) . ";\n?>");
 
-            $data=$res['access_token'];
+            $data = $res['access_token'];
         }
         return $data;
     }
 
-    public function getJSAPITicket(){
+    public function getJSAPITicket()
+    {
 
-        $name='ticket_'.md5($this->appid.$this->secret);
-        $filename=__DIR__.'/cache/'.$name.'.php';
-        if (is_file($filename)&&filemtime( + 7100 >time())){
-            $result=include $filename;
-            $data=$result['ticket'];
-        }else{
-            $url='https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$this->getAccessToken().'&type=jsapi';
-            $res=$this->curl($url);
-            $result=json_decode($res,true);
-            file_put_contents($filename,"<?php \n return ".var_export($result,true).";\n?>");
-            $data=$result['ticket'];
+        $name = 'ticket_' . md5($this->appid . $this->secret);
+        $filename = __DIR__ . '/cache/' . $name . '.php';
+        if (is_file($filename) && filemtime(+7100 > time())) {
+            $result = include $filename;
+            $data = $result['ticket'];
+        } else {
+            $url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=' . $this->getAccessToken() . '&type=jsapi';
+            $res = $this->curl($url);
+            $result = json_decode($res, true);
+            file_put_contents($filename, "<?php \n return " . var_export($result, true) . ";\n?>");
+            $data = $result['ticket'];
         }
         return $data;
     }
 
-    public function sign(){
-        $nonceStr=$this->makeStr();
-        $ticket=$this->getJSAPITicket();
-        $time=time();
-        $url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-        $arr=[
-            'noncestr='.$nonceStr,
-            'jsapi_ticket='.$ticket,
-            'timestamp='.$time,
-            'url='.$url,
+    public function sign()
+    {
+        $nonceStr = $this->makeStr();
+        $ticket = $this->getJSAPITicket();
+        $time = time();
+        $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+        $arr = [
+            'noncestr=' . $nonceStr,
+            'jsapi_ticket=' . $ticket,
+            'timestamp=' . $time,
+            'url=' . $url,
         ];
-        sort($arr,SORT_STRING);
-        $string=implode('&',$arr);
-        $sign=sha1($string);
-        $content=[
-          'appid'=>$this->appid,
-          'timestamp'=>$time,
-          'nonceStr'=>$nonceStr,
-          'signature' =>$sign
+        sort($arr, SORT_STRING);
+        $string = implode('&', $arr);
+        $sign = sha1($string);
+        $content = [
+            'appid' => $this->appid,
+            'timestamp' => $time,
+            'nonceStr' => $nonceStr,
+            'jsapi_ticket=' =>$ticket,
+            'url=' => $url,
+            'signature' => $sign
         ];
         return $content;
     }
 
-    protected function makeStr(){
-        $seed='qwe1r4tu5nj2j5i5opp1gv5ds2x8d7f9g5nbg3';
-        $str='';
-        for ($i=0;$i<16;$i++){
-            $num=rand(0,strlen($seed)-1);
-            $str.=$seed[$num];
+    protected function makeStr()
+    {
+        $seed = 'qwe1r4tu5nj2j5i5opp1gv5ds2x8d7f9g5nbg3';
+        $str = '';
+        for ($i = 0; $i < 16; $i++) {
+            $num = rand(0, strlen($seed) - 1);
+            $str .= $seed[$num];
         }
         return $str;
     }
