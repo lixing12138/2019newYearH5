08b3df501ef92bc0d6aa180f5b220cafed0681ef
lixing <16302010012@fudan.edu.cn>
2019-02-03 13:43:37 +0800

    final

--- js/index.js
+++ js/index.js
@@ -298,7 +298,7 @@ $(
         let shareTitle = '“叮咚｜复旦大学向你派送一份新年上上签”',//分享标题
             shareLink = 'https://xcx.fudan.edu.cn/newyear/',//分享链接
             shareDescription = '在猪年第一天悄然降临，睡眼惺松的你，准备好打开金光闪闪的许愿瓶了吗？',//分享描述
-            shareIcon = 'https://xcx.fudan.edu.cn/newyear/sharePic.png',//分享ICON;
+            shareIcon = 'https://xcx.fudan.edu.cn/newyear/share.png',//分享ICON;
             current_url = location.href.split('#')[0];
         function doShare() {
             $.get('https://xcx.fudan.edu.cn/newyear/php/getMessage.php?url=' + current_url, function (da) {
